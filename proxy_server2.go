cf83bd702f42adcd03a92ba0a298dc4aeab49cd3
qosimmax <qosimmax@gmail.com>
2016-08-19 01:01:13 +0500

    new verion of proxy server via NewSingleHostReverseProxy

---
+++ proxy_server2.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Content types
+var CONTENT_TYPES = []string{"html", "text", "json", "xml"}
+
+// regexp Compile
+var RP = regexp.MustCompile("[\\p{L}\\d_]+")
+
+// IsValidContent function check the Content-Type header
+func IsValidContent(ctype string) bool {
+	for _, v := range CONTENT_TYPES {
+		if strings.Contains(ctype, v) {
+			return true
+		}
+	}
+	return false
+}
+
+// replace old str to new str
+func Replace(data *[]byte, oldStr []byte, newStr []byte) {
+	// use a unicode character class to include the digits and underscore
+	*data = RP.ReplaceAllFunc(*data, func(b []byte) []byte {
+		if bytes.Equal(b, oldStr) {
+			return newStr
+		}
+		return b
+	})
+}
+
+// Decode gzip data
+func DecodeZip(b []byte) (z []byte, err error) {
+	reader, err := gzip.NewReader(bytes.NewReader(b))
+	if err != nil {
+		return z, err
+	}
+
+	z, err = ioutil.ReadAll(reader)
+	return
+}
+
+// gzip data
+func EncodeZip(b []byte) (z []byte, err error) {
+	var bz bytes.Buffer
+	gz := gzip.NewWriter(&bz)
+	if _, err = gz.Write(b); err != nil {
+		return
+	}
+	if err = gz.Flush(); err != nil {
+		return
+	}
+	if err = gz.Close(); err != nil {
+		return
+	}
+	z = bz.Bytes()
+	return
+}
+
+type transport struct {
+	http.RoundTripper
+	oldStr string
+	newStr string
+}
+
+func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	resp, err = t.RoundTripper.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// check the Content-Type header
+	if !IsValidContent(resp.Header.Get("Content-type")) {
+		return resp, nil
+	}
+
+	//read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	//close body
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// check the Content-Encoding
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		//decode gzip
+		body, err = DecodeZip(body)
+		if err != nil {
+			return nil, err
+		}
+		// Replace old str with new
+		Replace(&body, []byte(t.oldStr), []byte(t.newStr))
+
+		// encode to gzip
+		body, err = EncodeZip(body)
+		if err != nil {
+			return nil, err
+		}
+
+	default:
+		// Replace old str with new
+		Replace(&body, []byte(t.oldStr), []byte(t.newStr))
+
+	}
+
+	// set body and ContentLength
+	resp.Body = ioutil.NopCloser(bytes.NewReader(body))
+	resp.ContentLength = int64(len(body))
+	resp.Header.Set("Content-Length", strconv.Itoa(len(body)))
+
+	return resp, nil
+}
+
+// RerverseProxy object
+type Prox struct {
+	// target url of reverse proxy
+	target *url.URL
+	// instance of Go ReverseProxy
+	proxy *httputil.ReverseProxy
+}
+
+// New function create instance of Prox
+func New(target string) *Prox {
+	url, _ := url.Parse(target)
+	return &Prox{target: url, proxy: httputil.NewSingleHostReverseProxy(url)}
+}
+
+func (p *Prox) handle(w http.ResponseWriter, r *http.Request) {
+	r.Host = r.URL.Host
+	p.proxy.ServeHTTP(w, r)
+}
+
+func main() {
+	// constants
+	const (
+		Port          = ":3000"
+		defaultTarget = "geocode-maps.yandex.ru/1.x/?geocode=Москва,+Тверская+улица,+дом+7"
+		//defaultTarget      = "habrahabr.ru"
+		defaultTargetUsage = "default redirect url, '127.0.0.1:8080'"
+		defaultOldStr      = "Hi"
+		defaultOldStrUsage = "default value, 'Hi'"
+		defaultNewStr      = "Hello"
+		defaultNewStrUsage = "default new replacer value, 'Hello'"
+	)
+
+	// flags
+	host := flag.String("host", defaultTarget, defaultTargetUsage)
+	oldStr := flag.String("old", defaultOldStr, defaultOldStrUsage)
+	newStr := flag.String("new", defaultNewStr, defaultNewStrUsage)
+	flag.Parse()
+
+	//checking host url
+	*host = fmt.Sprintf("https://%v/", *host)
+	_, err := url.Parse(*host)
+	if err != nil {
+		log.Fatal("Error parsing URL")
+	}
+
+	fmt.Println("server listen on:", Port)
+	fmt.Println("redirecting to:", *host)
+	// proxy instance
+	proxy := New(*host)
+	proxy.proxy.Transport = &transport{http.DefaultTransport, *oldStr, *newStr}
+	http.HandleFunc("/", proxy.handle)
+	// start proxy server
+	http.ListenAndServe(Port, nil)
+}
