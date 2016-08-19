68e31f1273029e74e8d7be10000a6b39873640b6
Kasim <qosimmax@gmail.com>
2016-08-19 10:38:02 +0500

    test fixed

--- proxy_server_test.go
+++ proxy_server_test.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
 func TestContenType(t *testing.T) {
 
 	content_type := "text/html"
+
 	if !IsValidContent(content_type) {
 		t.Errorf("Invalid Content-Type:%v", content_type)
 	}
 
 }
 
-func ReplaceTest(t *testing.T) {
+func TestReplace(t *testing.T) {
 	data := []byte("Hello world")
 	oldStr := []byte("world")
 	newStr := []byte("World")
@@ -27,20 +30,51 @@ func ReplaceTest(t *testing.T) {
 
 }
 
-func DecodeZipTest(t *testing.T) {
+func TestDecodeZip(t *testing.T) {
+
 	data := []byte("Hello world")
-	_, err := DecodeZip(data)
+
+	gzdata, err := EncodeZip(data)
 	if err != nil {
-		t.Error("Decode gzip data error")
+		t.Errorf("Encode data error:%v", err)
+		return
+	}
+
+	_, err = DecodeZip(gzdata)
+	if err != nil {
+		t.Errorf("Decode gzip data error:%v", err)
 	}
 
 }
 
 func EncodeZipTest(t *testing.T) {
 	data := []byte("Hello world")
+
 	_, err := EncodeZip(data)
 	if err != nil {
 		t.Error("Encode data error")
 	}
 
+}
+
+func TestHandler(t *testing.T) {
+	oldStr := "Go"
+	newStr := "Golang"
+
+	tr := &transport{http.DefaultTransport, oldStr, newStr}
+	c := &http.Client{Transport: tr}
+	resp, err := c.Get("http://habrahabr.ru")
+	if err != nil {
+		t.Errorf("http request error:%v", err)
+		return
+	}
+	//read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("read response error:%v", err)
+		return
+	}
+	if !bytes.Contains(body, []byte(newStr)) {
+		t.Errorf("'%v' not replaced", newStr)
+	}
 }
